ab9ac38abd9204c85495b4d4549a05745fcebe2b
Bradley Woods <BradleyWoods@spartaglobal.com>
2023-05-10 09:53:30 +0100

    Added 'automate_the_boring_stuff

---
+++ automate_the_boring_stuff/debugging.py
@@ -0,0 +1,50 @@
+import logging
+logging.disable(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
+logging.debug('Start of program')
+
+def factorial(n):
+    logging.debug('Start of factorial(%s%%)'  % (n))
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of factorial(%s%%)'  % (n))
+    return total
+
+# def boxPrint(symbol, width, height):
+#     if len(symbol) != 1:
+#         raise Exception('Symbol must be a single character string.')
+#     if width <= 2:
+#         raise Exception('Width must be greater than 2.')
+#     if height <= 2:
+#         raise Exception('Height must be greater than 2.')
+
+#     print(symbol * width)
+#     for i in range(height - 2):
+#         print(symbol + (' ' * (width - 2)) + symbol)
+#     print(symbol * width)
+
+# def switchLights(stoplight):
+#     for key in stoplight.keys():
+#         if stoplight[key] == 'green':
+#             stoplight[key] = 'yellow'
+#         elif stoplight[key] == 'yellow':
+#             stoplight[key] = 'red'
+#         elif stoplight[key] == 'red':
+#             stoplight[key] = 'green'
+#         assert 'red' in stoplight.values(), 'Neither light is red! ' + str(stoplight)
+
+print(factorial(5))
+logging.debug('End of program')
+
+# for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+#     try:
+#         boxPrint(sym, w, h)
+#     except Exception as err:
+#         print('An exception happened: ' + str(err))
+
+# market_2nd = {'ns': 'green', 'ew': 'red'}
+# mission_16th = {'ns': 'red', 'ew': 'green'}
+
+# switchLights(market_2nd)
